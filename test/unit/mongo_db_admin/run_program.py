0d29f693445041d5bb8f5cbe9f8ca643489d4ca7
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-10-15 18:05:14 +0100

    Update test/unit/mongo_db_admin/run_program.py

--- test/unit/mongo_db_admin/run_program.py
+++ test/unit/mongo_db_admin/run_program.py
@@ -57,10 +57,6 @@ class Server(object):
 
     Description:  Class stub holder for mongo_class.Server class.
 
-    Super-Class:
-
-    Sub-Classes:
-
     Methods:
         __init__ -> Class initialization.
         connect -> Stub holder for mongo_class.Server.connect method.
@@ -97,10 +93,6 @@ class UnitTest(unittest.TestCase):
     """Class:  UnitTest
 
     Description:  Class which is a representation of a unit testing.
-
-    Super-Class:  unittest.TestCase
-
-    Sub-Classes:
 
     Methods:
         setUp -> Initialize testing environment.
