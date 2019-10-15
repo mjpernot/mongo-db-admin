2974acdffa7ed57c09e558548c3c57034a1039ed
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-10-15 18:03:47 +0100

    Update test/unit/mongo_db_admin/repair_db.py

--- test/unit/mongo_db_admin/repair_db.py
+++ test/unit/mongo_db_admin/repair_db.py
@@ -53,10 +53,6 @@ class Server(object):
 
     Description:  Class stub holder for mongo_class.Server class.
 
-    Super-Class:
-
-    Sub-Classes:
-
     Methods:
         __init__ -> Class initialization.
 
@@ -80,10 +76,6 @@ class UnitTest(unittest.TestCase):
     """Class:  UnitTest
 
     Description:  Class which is a representation of a unit testing.
-
-    Super-Class:  unittest.TestCase
-
-    Sub-Classes:
 
     Methods:
         setUp -> Initialize testing environment.
