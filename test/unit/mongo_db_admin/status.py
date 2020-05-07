855a2f69fe9e03fdf60cac90f906e2774f0538b0
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-05-07 09:02:27 +0100

    Update test/unit/mongo_db_admin/status.py v2.3.1

--- test/unit/mongo_db_admin/status.py
+++ test/unit/mongo_db_admin/status.py
@@ -71,7 +71,8 @@ class Mail(object):
 
         """
 
-        pass
+        self.lag_time = lag_time
+        self.data = None
 
     def add_2_msg(self, data):
 
@@ -82,6 +83,8 @@ class Mail(object):
         Arguments:
 
         """
+
+        self.data = data
 
         return True
 
