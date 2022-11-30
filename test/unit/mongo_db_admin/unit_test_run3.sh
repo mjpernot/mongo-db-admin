5cd6a7149c4fa858a2d06595bbece0f577724c10
Mark Pernot <mark.pernot@gmail.com>
2022-11-30 11:23:44 +0000

    Update unit test modules v2.4.3

---
+++ test/unit/mongo_db_admin/unit_test_run3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Unit testing program for the mongo_db_admin.py module.
+# This will run all the units tests for this program.
+# Will need to run this from the base directory where the module file 
+#   is located at.
+
+echo ""
+echo "Unit test:"
+/usr/bin/python3 test/unit/mongo_db_admin/compact.py
+/usr/bin/python3 test/unit/mongo_db_admin/dbcc.py
+/usr/bin/python3 test/unit/mongo_db_admin/defrag.py
+/usr/bin/python3 test/unit/mongo_db_admin/get_log.py
+/usr/bin/python3 test/unit/mongo_db_admin/help_message.py
+/usr/bin/python3 test/unit/mongo_db_admin/main.py
+/usr/bin/python3 test/unit/mongo_db_admin/process_dbs_tbls.py
+/usr/bin/python3 test/unit/mongo_db_admin/process_mail.py
+/usr/bin/python3 test/unit/mongo_db_admin/process_request.py
+/usr/bin/python3 test/unit/mongo_db_admin/rotate.py
+/usr/bin/python3 test/unit/mongo_db_admin/run_compact.py
+/usr/bin/python3 test/unit/mongo_db_admin/run_dbcc.py
+/usr/bin/python3 test/unit/mongo_db_admin/run_program.py
+/usr/bin/python3 test/unit/mongo_db_admin/status.py
